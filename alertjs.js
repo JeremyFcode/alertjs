fe94b385e25644bd196a1beabde2d76e4cd6820b
benjaminb- <contact@benjaminb.fr>
2013-01-26 19:13:53 +0100

    mistake

--- alertjs.js
+++ alertjs.js
@@ -103,9 +103,6 @@
         _close_btn.addEventListener("click",function(){
             _body.removeChild(_box);
             _body.removeChild(_layer);
-            if(typeof urlToGo == 'string'){
-                window.location.href = urlToGo;
-            }
         }, false);
         
     }
